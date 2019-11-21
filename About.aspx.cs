65aad3f15d6cbcdc7241ddeb81055d7b5a6bd254
shikhagoyal96 <shikha9.6goyal@gmail.com>
2019-11-21 11:31:32 -0500

    Add project files.

---
+++ About.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HTTP_5101_FinalProject_N01329988
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTTP_5101_FinalProject_N01329988.Global" Language="C#" %>
